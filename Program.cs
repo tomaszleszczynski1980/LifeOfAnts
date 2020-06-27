1f9e3c5a4b756620d4f5bf4abb44bc250edb4a74
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-06-27 14:36:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LifeOfAnts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
